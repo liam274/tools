ad522fdbaa8725af7e5926fdebac77bd81b69107
soft-compile <ezyjsdeveloper@gmail.com>
2026-05-02 07:55:31 +0800

    fix

--- random-text.py
+++ random-text.py
@@ -7,15 +7,20 @@ if len(sys.argv)<3:
     print("Usage: ./random-text.py <file-name> <file-length>")
     sys.exit(1)
 
-os.path.exists(sys.argv[1]) and print("Error: <file-name> must be a non-exist path")==None and sys.exit(1)
+FILENAME: str=sys.argv[1]
+FILELENGTH: str=sys.argv[2]
 
-times: int=int(sys.argv[2] if sys.argv[2].isdigit() else (sys.exit(1) if print("Error: <file-length> must be an available dec")==None else None))
+result: str=""
+os.path.exists(FILENAME) and print("Error: <file-name> must be a non-exist path",end="")==None and (result:=input("(c for \"cover\", q for \"quit\")"))!="c" and sys.exit(1)
+result=="c" and open(FILENAME, "w").close() and print(f"File {FILENAME} cleared")
 
-LETTERS: str="".join(i for i in range("a","z"+1))+"".join(i for i in range("A","Z"+1))+"0123456789, ;.?"
+times: int=int(FILELENGTH if FILELENGTH.isdigit() else (sys.exit(1) if print("Error: <file-length> must be an available dec")==None else None))
+
+LETTERS: str="".join(chr(i) for i in range(ord("a"),ord("z")+1))+"".join(chr(i) for i in range(ord("A"),ord("Z")+1))+"0123456789, ;.?"
 def random_text():
-    return "\n".join("".join(random.choice(LETTERS) for n in range(i)) for i in range(random.randint(20,50)))
+    return "".join("".join(random.choice(LETTERS) for _ in range(i)) for i in range(random.randint(20,50)))
 
-with open(sys.argv[1],"a+") as file:
+with open(FILENAME,"a+") as file:
     for i in range(times):
         print(f"line {i}:",random_text(),file=file)
     print(file=file,flush=True)
