29d2b95336cc516f0e170e94b4f3f71f73488135
soft-compile <ezyjsdeveloper@gmail.com>
2026-05-02 15:14:00 +0800

    fix

--- random-text.py
+++ random-text.py
@@ -11,14 +11,14 @@ FILENAME: str=sys.argv[1]
 FILELENGTH: str=sys.argv[2]
 
 result: str=""
-os.path.exists(FILENAME) and print("Error: <file-name> must be a non-exist path",end="")==None and (result:=input("(c for \"cover\", q for \"quit\")"?))!="c" and sys.exit(1)
+os.path.exists(FILENAME) and print("Error: <file-name> must be a non-exist path",end="")==None and (result:=input("(c for \"cover\", q for \"quit\")?"))!="c" and sys.exit(1)
 result=="c" and open(FILENAME, "w").close() and print(f"File {FILENAME} cleared")
 
 times: int=int(FILELENGTH if FILELENGTH.isdigit() else (sys.exit(1) if print("Error: <file-length> must be an available dec")==None else None))
 
 LETTERS: str="".join(chr(i) for i in range(ord("a"),ord("z")+1))+"".join(chr(i) for i in range(ord("A"),ord("Z")+1))+"0123456789, ;.?"
 def random_text():
-    return "".join("".join(random.choice(LETTERS) for _ in range(i)) for i in range(random.randint(20,50)))
+    return "".join(random.choice(LETTERS) for i in range(random.randint(20,500)))
 
 with open(FILENAME,"a+") as file:
     for i in range(times):
